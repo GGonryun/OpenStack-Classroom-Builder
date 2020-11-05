63a67867dc89e8a879d07bb7a5633057ccb70e7f
Miguel Campos <amodestduck@gmail.com>
2020-11-05 07:54:14 -0800

    update properties

--- engine.py
+++ engine.py
@@ -43,7 +43,7 @@ ledger = {}
 def start():
   # Meat & Substance: This function is the "main" function of the file, it creates vm's for users from a csv file.
   [classroom, _] = read_yaml(CLASSROOM_FILE)
-  projects = classroom.Project
+  projects = classroom[PROJECT]
   ledger[DOMAIN] = create_domain.create_domain('default')
   ledger[PROJECT] = create_project.create_project(projects, ADMIN, ADMIN)
   ledger[NETWORKS] = map(partial(create_network.create_linked_network, ledger[DOMAIN], ledger[PROJECT]), classroom[NETWORKS])
