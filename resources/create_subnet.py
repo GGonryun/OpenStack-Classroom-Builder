3be7417bcb1e81f7eee8beb47c8d1c34910fba3e
Miguel Campos <amodestduck@gmail.com>
2020-11-06 07:44:45 -0800

    remove name from subnet creation

--- resources/create_subnet.py
+++ resources/create_subnet.py
@@ -10,7 +10,7 @@ def create_subnet(domain, project, name, network_id, cidr):
   print("\tcreate_subnet(domain, project, name, network_id, cidr): {}, {}, {}, {}, {}".format(domain, project, name, network_id, cidr))
   try:
     client = users_utility.create_neutron_client(domain, project)
-    subnet = client.create_subnet({'subnet': {'name': name, 'network_id': network_id, 'ip_version': 4, 'cidr': cidr }})
+    subnet = client.create_subnet({'subnet': {'network_id': network_id, 'ip_version': 4, 'cidr': cidr }})
     return subnet.id
   except Exception as ex:
       print("\tcreate_subnet: an error occured:, {}, {}, {}, {}, {}, {}", ex, domain, project, name, network_id, cidr)
