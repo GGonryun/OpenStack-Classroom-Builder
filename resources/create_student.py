62378a51105df8a8826fb1bb02227dd4ca2a50b1
Miguel Campos <amodestduck@gmail.com>
2020-11-06 14:30:14 -0800

    debugging machines: create machines 13

--- resources/create_student.py
+++ resources/create_student.py
@@ -15,6 +15,6 @@ def create_student(ledger, student):
   student[PROJECT] = project
   student[PASSWORD] = users_utility.generate_user_password(username)
   s = create_student_users.create_student_users([student])[0]
-  student[ID] = s['id']
+  student[ID] = s.id
   # returns an array, we access the first element because we only create 1 student at a time.
   return student
