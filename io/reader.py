90c0eecff6cbcc1f2df2ebff1b7f8b1973f544d2
Miguel Campos <amodestduck@gmail.com>
2020-11-11 14:47:09 -0800

    ignore missing keys

--- io/reader.py
+++ io/reader.py
@@ -46,8 +46,10 @@ def clean_up(data):
     
   if(machines != None):
     print("\t\tclean_up: cleaning up machines".format(machines))
-    clone[MACHINES_KEY][PER_PROJECT_KEY] = list(filterNonEmpty(map(machine_csv_to_dict, clone[MACHINES_KEY][PER_PROJECT_KEY])))
-    clone[MACHINES_KEY][PER_USER_KEY] = list(filterNonEmpty(map(machine_csv_to_dict, clone[MACHINES_KEY][PER_USER_KEY])))
+    if clone[MACHINES_KEY][PER_PROJECT_KEY]:
+      clone[MACHINES_KEY][PER_PROJECT_KEY] = list(filterNonEmpty(map(machine_csv_to_dict, clone[MACHINES_KEY][PER_PROJECT_KEY])))
+    if clone[MACHINES_KEY][PER_USER_KEY]:
+      clone[MACHINES_KEY][PER_USER_KEY] = list(filterNonEmpty(map(machine_csv_to_dict, clone[MACHINES_KEY][PER_USER_KEY])))
 
   return clone
 
