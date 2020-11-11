a730c0362e06b24a34517430ee8003ed3af6e898
Miguel Campos <amodestduck@gmail.com>
2020-11-11 15:00:43 -0800

    edgecase

--- io/reader.py
+++ io/reader.py
@@ -72,7 +72,7 @@ def machine_csv_to_dict(csv):
   print("\t\tmachine_csv_to_dict: converting csv into dictionary {}".format(csv))
   arr = csv.split(',')
   access = partial(safeAccess, arr)
-  if(len(arr) > 5):
+  if(len(arr) >= 5):
     machine = { "Name": access(0), "Instances": access(1), "Image": access(2), "Flavor": access(3), "Network": access(4), "Password": access(5) }
     print("\t\tmachine_csv_to_dict: created machine {}".format(machine))
     return machine
